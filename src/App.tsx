c7a408341dc8305d0c66cb49ed4e91efb4911a62
Pablo Gabriel Perera <pgp.2013.88@gmail.com>
2021-10-19 16:10:38 -0300

    intitial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ASSET_URL, CHOSEN_THEME } from './constants'
+import logo from './static/images/logo.png';
+import './static/styles/App.css';
+import footer from './static/images/footer.png';
+import { iframeResizer } from 'iframe-resizer'
+import './static/styles/App.css';
+
+const embeddedUrl = `${ASSET_URL}?embed=${CHOSEN_THEME}`
+
+class App extends React.Component {
+
+  componentDidMount() {
+    iframeResizer({ log: false }, '#opensea-iframe')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            
+          </p>
+          <i className="material-icons right">menu</i>
+        </header>
+        <main className="App-main">
+          <div className="App-hero">
+            <div className="App-hero-image"></div>
+            <p>
+              Grape Marketplace
+            </p>
+            <small>NFTs para potenciar tus ideas.</small>
+          </div>
+          <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} width='2000px' height='2000px%' frameBorder='0' allowFullScreen></iframe>
+        </main>
+        <footer className="App-footer">
+          {/* Placeholder image for footer */}
+          <img style={{width: '100%'}} src={footer} className="footer" alt="footer" />
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const ASSET_URL =
+  "https://opensea.io/collection/the-worlds-most-valuable-rare-stamps";
+  // "https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/83697397869601953593385986599331272042560746529830664688448075657192086175746";
+export enum EmbedTheme {
+  Default = "true",
+  Simple = "simple",
+}
+
+export const CHOSEN_THEME = EmbedTheme.Default;
