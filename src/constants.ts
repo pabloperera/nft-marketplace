68c1a6757467a94b186ddf207d8dfca7f0008117
Pablo Gabriel Perera <pgp.2013.88@gmail.com>
2021-10-21 19:56:09 -0300

    Se agrega fondo dinamico

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 export const ASSET_URL =
-  "https://opensea.io/collection/the-worlds-most-valuable-rare-stamps";
+  // "https://opensea.io/collection/the-worlds-most-valuable-rare-stamps";
   // "https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/83697397869601953593385986599331272042560746529830664688448075657192086175746";
-export enum EmbedTheme {
+  "https://opensea.io/assets/0x359fb071477a9ad82835768f5ce9a29a0e5c1575/1303";
+  export enum EmbedTheme {
   Default = "true",
   Simple = "simple",
 }
