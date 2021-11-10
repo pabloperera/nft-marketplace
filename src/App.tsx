b438ee368be589b2a7a655f80ffbc0b0e4da8150
njdiazsoler <njdiazsoler@figandgrape.io>
2021-11-10 19:18:17 -0300

    migrated constants, fixed cards, added header and body background colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { ASSET_URL } from "./constants";
+import { options } from "./constants";
 import logo from "./static/images/logo.png";
 import footer from "./static/images/footer.png";
 import { iframeResizer } from "iframe-resizer";
-import {
-  Card,
-  Col,
-  Container,
-  Image,
-  Row,
-} from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import "./static/styles/App.css";
 
-const options = [
-  {
-    title: "Soraya",
-    collectionUrl:
-      "https://opensea.io/collection/kaiju-kingz",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/0/08/Soraya_Arnelas_3.jpg",
-  },
-  {
-    title: "Il Divo",
-    collectionUrl:
-      "https://opensea.io/collection/jungle-freaks-by-trosley",
-    imageUrl:
-      "https://denverconvention.com/uploads/content/ILDIVOPRESSSHOTSTAGE(1).jpg",
-  },
-  {
-    title: "Pink Panthers",
-    collectionUrl:
-      "https://opensea.io/collection/doodles-official",
-    imageUrl:
-      "https://vistapointe.net/images/pink-panther-wallpaper-2.jpg",
-  },
-  {
-    title: "NFT 3D",
-    collectionUrl:
-      "https://opensea.io/collection/cryptopunks",
-    imageUrl:
-      "https://491569-1551798-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/09/Nu_ComputerGirl_22.png",
-  },
-  {
-    title: "Fig & Grape",
-    collectionUrl:
-      "https://opensea.io/collection/boredapeyachtclub",
-    imageUrl:
-      "https://thumbs.dreamstime.com/b/fig-white-grape-10911840.jpg",
-  },
-  {
-    title: "Stamps",
-    collectionUrl: ASSET_URL,
-    imageUrl:
-      "https://fthmb.tqn.com/1LBGKoLrGTPYGRa2cGZWV1jY4RM=/1280x853/filters:fill(auto,1)/166963448-F-56b005105f9b58b7d01f7e61.jpg",
-  },
-];
+
 
 const App = () => {
   const [collectionUrl, setCollectionUrl] = useState("");
   useEffect(() => {
-    console.log("resizing");
+    // console.log("resizing");
   });
 
   return (
@@ -83,11 +35,36 @@ const App = () => {
                 {options.map((el) => {
                   return (
                     <Col
-                      md={4}
+                      md={el.title.match(/Soraya|Il Divo/gi) ? 12 : 6}
                       style={{ margin: "20px 0px" }}
                     >
-                      <Card>
-                        <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', height: '300px' }}>
+                      <Card
+                        onClick={() => {
+                          // setOpenModal(true);D
+                          setCollectionUrl(el.collectionUrl);
+                        }}
+                      >
+                        <div
+                          style={{
+                            alignItems: "flex-end",
+                            display: "flex",
+                            justifyContent: "center",
+                            background: `url(${el.imageUrl})`,
+                            backgroundPosition: el.backgroundPosition || "auto",
+                            backgroundSize: el.backgroundSize || "auto",
+                            height: "300px",
+                          }}
+                        >
+                          <span
+                            style={{
+                              fontSize: "40px",
+                              fontWeight: 800,
+                              textShadow: "-2px 2px black",
+                              color: "white",
+                            }}
+                          >
+                            {el.title}
+                          </span>
                           {/* <Image
                             alt={`${el.title} Collection Background`}
                             style={{
@@ -108,27 +85,22 @@ const App = () => {
                               );
                             }}
                           /> */}
-                          <Image
-                          alt={`${el.title} Collection`}
+                          {/* <Image
+                            alt={`${el.title} Collection`}
                             style={{
-                              background: 'transparent',
+                              background: "transparent",
                               height: "270px",
                               objectFit: "contain",
-                              width: '100%',
+                              width: "100%",
                               zIndex: 100,
                             }}
                             src={el.imageUrl}
                             onClick={() => {
                               // setOpenModal(true);D
-                              setCollectionUrl(
-                                el.collectionUrl
-                              );
+                              setCollectionUrl(el.collectionUrl);
                             }}
-                          />
+                          /> */}
                         </div>
-                        <Card.Header>
-                          {el.title}
-                        </Card.Header>
                       </Card>
                     </Col>
                   );
@@ -145,12 +117,7 @@ const App = () => {
                 height="2000px"
                 frameBorder="0"
                 allowFullScreen
-                onLoad={() =>
-                  iframeResizer(
-                    { log: false },
-                    "#opensea-iframe"
-                  )
-                }
+                onLoad={() => iframeResizer({ log: false }, "#opensea-iframe")}
               ></iframe>
             </div>
           )}
--- src/constants.ts
+++ src/constants.ts
@@ -7,4 +7,49 @@ export const ASSET_URL =
   Simple = "simple",
 }
 
+export const options = [
+  {
+    backgroundPosition: "50% 60%",
+    backgroundSize: "1000px",
+    collectionUrl: "https://opensea.io/collection/kaiju-kingz",
+    imageUrl:
+      "https://mix931fm.com/files/2017/05/concert-crowd-ThinkstockPhotos-577332304.jpg",
+    title: "Soraya",
+  },
+  {
+    backgroundPosition: "50% 90%",
+    backgroundSize: "1500px",
+    collectionUrl: "https://opensea.io/collection/jungle-freaks-by-trosley",
+    imageUrl:
+      "http://www.cambridgeblog.org/wp-content/uploads/2015/11/Music-and-the-Exotic-from-the-Renaissance-to-Mozart.jpg",
+    title: "Il Divo",
+  },
+  {
+    backgroundSize: "cover",
+    collectionUrl: "https://opensea.io/collection/doodles-official",
+    imageUrl: "https://vistapointe.net/images/pink-panther-wallpaper-2.jpg",
+    title: "Pink Panthers",
+  },
+  {
+    backgroundSize: "cover",
+    collectionUrl: "https://opensea.io/collection/cryptopunks",
+    imageUrl:
+    "https://491569-1551798-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/09/Nu_ComputerGirl_22.png",
+    title: "NFT 3D",
+  },
+  {
+    backgroundSize: "cover",
+    collectionUrl: "https://opensea.io/collection/boredapeyachtclub",
+    imageUrl: "https://thumbs.dreamstime.com/b/fig-white-grape-10911840.jpg",
+    title: "Fig & Grape",
+  },
+  {
+    backgroundSize: "cover",
+    collectionUrl: "https://opensea.io/assets/0x359fb071477a9ad82835768f5ce9a29a0e5c1575/1303",
+    imageUrl:
+    "https://fthmb.tqn.com/1LBGKoLrGTPYGRa2cGZWV1jY4RM=/1280x853/filters:fill(auto,1)/166963448-F-56b005105f9b58b7d01f7e61.jpg",
+    title: "Stamps",
+  },
+];
+
 export const CHOSEN_THEME = EmbedTheme.Default;
