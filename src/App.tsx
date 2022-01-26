ebc0c28c2857220d1cf7cbd6fde75effcbcd3c10
Pablo Perera <pabloperera@figandgrape.io>
2022-01-26 15:17:46 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import logofooter from "./static/images/logo-footer.png";
 import { iframeResizer } from "iframe-resizer";
 import { Card, Col, Container, Row, Navbar, Nav, Carousel} from "react-bootstrap";
 import "./static/styles/App.css";
+import "./static/styles/style.scss";
 import { SocialIcon } from 'react-social-icons';
 
 const App = () => {
@@ -59,7 +60,151 @@ const App = () => {
           </Container>
         </div>
         <h1 className="App-titulo-intermedio">Exclusive collections</h1>
-        <div className="App-content-format">
+        
+        {/* card 1 */}
+        <div className="App-card-new"> 
+        <article className="postcard dark blue">
+          <a className="postcard__img_link" href="https://nft.figandgrape.io">
+            <img className="postcard__img" src="https://i.ibb.co/PFvFnjr/arboreum56.jpg" alt="Title" />
+          </a>
+          <div className="postcard__text">
+            <h1 className="postcard__title blue"><a href="https://nft.figandgrape.io">Arboreum</a></h1>
+            <div className="postcard__subtitle small">
+              {/* <time datetime="2020-05-25 12:00:00"> */}
+              <time>
+                <i className="fas fa-calendar-alt mr-2"></i>Realease on Sat, Feb 2th 2022
+              </time>
+            </div>
+            <div className="postcard__bar"></div>
+            <div className="postcard__preview-txt">
+            Desde su juventud ha sido una persona inquieta a la que le han interesado las disciplinas que, de alguna forma, pudieran ayudar al encuentro de la paz interior de las personas, teniendo siempre la naturaleza arbórea como fuente de inspiración de sus obras dentro del proyecto Zensations & Arbóreum.              </div>
+            <ul className="postcard__tagbox">
+              <li className="tag__item"><i className="fas fa-tag mr-2"></i>Albert Cuevas</li>
+              <li className="tag__item"><i className="fas fa-clock mr-2"></i>20 NFTs</li>
+              <li className="tag__item play blue">
+                <a href="https://nft.figandgrape.io"><i className="fas fa-play mr-2"></i>Watch Collection</a>
+              </li>
+            </ul>
+          </div>
+          <a className="postcard__img_link" href="https://nft.figandgrape.io">
+            <img className="postcard__img" src="https://i.ibb.co/R72XnBM/beto-logo.jpg" alt="Title" />
+          </a>
+        </article>
+        </div>
+
+        <div className="App-espacio-inter"></div>
+
+        {/* card 2 */}
+        <div className="App-card-new"> 
+              <article className="postcard dark red">
+            <a className="postcard__img_link" href="https://nft.figandgrape.io">
+              <img className="postcard__img" src="https://i.ibb.co/8xXxYGs/carlos-marin-02.jpg" alt="Title" />	
+            </a>
+            <div className="postcard__text">
+              <h1 className="postcard__title red"><a href="https://nft.figandgrape.io">Carlos Marín</a></h1>
+              <div className="postcard__subtitle small">
+                <time>
+                  <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+                </time>
+              </div>
+              <div className="postcard__bar"></div>
+              <div className="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+              <ul className="postcard__tagbox">
+                <li className="tag__item"><i className="fas fa-tag mr-2"></i>Podcast</li>
+                <li className="tag__item"><i className="fas fa-clock mr-2"></i>55 mins.</li>
+                <li className="tag__item play red">
+                  <a href="https://nft.figandgrape.io"><i className="fas fa-play mr-2"></i>Play Episode</a>
+                </li>
+              </ul>
+            </div>
+            <a className="postcard__img_link" href="https://nft.figandgrape.io">
+              <img className="postcard__img" src="https://phantom-marca.unidadeditorial.es/0b06d605e943e75388f9264e7479ee40/resize/660/f/webp/assets/multimedia/imagenes/2021/12/17/16397178132444.jpg" alt="Title" />	
+            </a>
+          </article>
+        </div>
+       {/* card 3 */}
+       <div className="App-card-new"> 
+       <article className="postcard dark green">
+			<a className="postcard__img_link" href="https://nft.figandgrape.io">
+				<img className="postcard__img" src="https://i.ibb.co/SQbPw0s/pipanths-banner1.jpg" alt="Title" />
+			</a>
+			<div className="postcard__text">
+				<h1 className="postcard__title green"><a href="https://nft.figandgrape.io">Pipaths</a></h1>
+				<div className="postcard__subtitle small">
+					<time>
+						<i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div className="postcard__bar"></div>
+				<div className="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul className="postcard__tagbox">
+					<li className="tag__item"><i className="fas fa-tag mr-2"></i>Podcast</li>
+					<li className="tag__item"><i className="fas fa-clock mr-2"></i>55 mins.</li>
+					<li className="tag__item play green">
+						<a href="https://nft.figandgrape.io"><i className="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+      <a className="postcard__img_link" href="https://nft.figandgrape.io">
+				<img className="postcard__img" src="      https://i.ibb.co/prp838h/pipanthsap-Mesa-de-trabajo-1.png" alt="Title" />
+			</a>
+		</article>
+       </div>
+       {/* card 4 */}
+       <div className="App-card-new"> 
+       <article className="postcard dark yellow">
+			<a className="postcard__img_link" href="https://nft.figandgrape.io">
+				<img className="postcard__img" src="https://i.ibb.co/BKgFyTN/Captura.jpg" alt="Title" />
+			</a>
+			<div className="postcard__text">
+				<h1 className="postcard__title yellow"><a href="https://nft.figandgrape.io">Gonzalo</a></h1>
+				<div className="postcard__subtitle small">
+					<time>
+						<i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div className="postcard__bar"></div>
+				<div className="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul className="postcard__tagbox">
+					<li className="tag__item"><i className="fas fa-tag mr-2"></i>Podcast</li>
+					<li className="tag__item"><i className="fas fa-clock mr-2"></i>55 mins.</li>
+					<li className="tag__item play yellow">
+						<a href="https://nft.figandgrape.io"><i className="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+      <a className="postcard__img_link" href="https://nft.figandgrape.io">
+				<img className="postcard__img" src="https://i.ibb.co/cJjDXVT/Captura2.jpg" alt="Title" />
+			</a>
+		</article>
+       </div>
+        {/* card 5 */}
+        {/* <div className="App-card-new"> 
+        <article className="postcard light blue">
+			<a className="postcard__img_link" href="https://nft.figandgrape.io">
+				<img className="postcard__img" src="https://picsum.photos/1000/1000" alt="Title" />
+			</a>
+			<div className="postcard__text t-dark">
+				<h1 className="postcard__title blue"><a href="https://nft.figandgrape.io">Podcast Title</a></h1>
+				<div className="postcard__subtitle small">
+					<time>
+						<i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div className="postcard__bar"></div>
+				<div className="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
+				<ul className="postcard__tagbox">
+					<li className="tag__item"><i className="fas fa-tag mr-2"></i>Podcast</li>
+					<li className="tag__item"><i className="fas fa-clock mr-2"></i>55 mins.</li>
+					<li className="tag__item play blue">
+						<a href="https://nft.figandgrape.io"><i className="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+        </div> */}
+
+        {/* <div className="App-content-format">
           {collectionUrl === "" ? (
             <Container>
               <Row>
@@ -119,7 +264,9 @@ const App = () => {
                 ></iframe>
               </div>
           )}
-        </div>
+        </div> */}
+
+        
         <div className="App-coming-soon">
           <h1 className="App-titulo-intermedio">Coming soon</h1>
           <Container>
